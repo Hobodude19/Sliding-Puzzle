c89497344247b155fbad936cde095e8673681cdc
Hobodude19 <22244792+Hobodude19@users.noreply.github.com>
2020-01-05 00:24:32 -0500

    Add files via upload

--- Sliding Puzzle/src/Graphics/Window.java
+++ Sliding Puzzle/src/Graphics/Window.java
@@ -1,5 +1,22 @@
 package Graphics;
 
-public class Window {
+import javax.swing.*;
+import java.awt.*;
 
+public class Window {
+    JFrame frame;
+
+    public Window() {
+        frame = new JFrame("Sliding Puzzle");
+
+        frame.setBackground(Color.BLACK);
+        frame.setSize(1200,900);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        showFrame();
+    }
+
+    private void showFrame() {
+        frame.setVisible(true);
+    }
 }
