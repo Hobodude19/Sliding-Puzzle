37f5b07725885a33e9ae6424fa9b56d5079c8b4b
Hobodude19 <22244792+Hobodude19@users.noreply.github.com>
2020-01-05 01:06:13 -0500

    Added music

--- Sliding Puzzle/src/Graphics/Window.java
+++ Sliding Puzzle/src/Graphics/Window.java
@@ -1,18 +1,25 @@
 package Graphics;
 
+import Logic.Music;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Window {
     JFrame frame;
+    Music m;
 
     public Window() {
         frame = new JFrame("Sliding Puzzle");
 
         frame.getContentPane().setBackground(new Color(56,52,55));
         frame.setSize(1200,900);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+
+        m = new Music();
+        m.startMusic();
 
         showFrame();
     }
