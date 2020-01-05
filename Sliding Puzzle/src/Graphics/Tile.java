26506f9e5db0674b79c31bb2a6b438675e2a9f7b
Hobodude19 <22244792+Hobodude19@users.noreply.github.com>
2020-01-05 18:55:50 -0500

    Changed gen to make 15 tiles. Added mouselistener

--- Sliding Puzzle/src/Graphics/Tile.java
+++ Sliding Puzzle/src/Graphics/Tile.java
@@ -1,17 +1,19 @@
 package Graphics;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.*;
 
 public class Tile extends JPanel{
     int number;
     JLabel label;
-
     Color c;
+
     Tile(int number) {
         this.number = number;
         this.setLayout(new GridBagLayout());
-        
+
         label = new JLabel(Integer.toString(number));
         label.setFont(label.getFont().deriveFont(100f));
         this.add(label);
--- Sliding Puzzle/src/Graphics/Window.java
+++ Sliding Puzzle/src/Graphics/Window.java
@@ -4,6 +4,8 @@ import Logic.Music;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class Window {
     JFrame frame;
@@ -28,31 +30,52 @@ public class Window {
         panel.setPreferredSize(new Dimension(panelWidth,panelHeight));
         panel.setBackground(new Color(56,52,55));
         panel.setLayout(new GridBagLayout());
-        
+
         inner = new JPanel();
         inner.setPreferredSize(new Dimension(panelWidth-20,panelHeight-20));
         inner.setBackground(new Color(56,52,55));
         inner.setLayout(new GridLayout(4,4,10,10));
         panel.add(inner);
-        
-        
-        int number = 1;
+
+
+//        int number = 1;
 //        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 16; j++){
-                Tile tile = new Tile(number);
-                tile.setBackground(new Color(35,54,63).brighter());
-                tile.setPreferredSize(new Dimension());
-                inner.add(tile);
-                number++;
-            }
-        
-        
+        for(int j = 1; j < 16; j++){
+            Tile tile = new Tile(j);
+            tile.setBackground(new Color(35,54,63).brighter());
+            tile.setPreferredSize(new Dimension());
+            tile.addMouseListener(new MouseListener(){
+                @Override
+                public void mouseClicked(MouseEvent e) {
+
+                }
+                public void mousePressed(MouseEvent e) {
+                    System.out.println(tile.number+" was clicked!");
+                }
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                }
+                @Override
+                public void mouseEntered(MouseEvent e) {
+
+                }
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+            });
+            inner.add(tile);
+//            number++;
+        }
+
+
         frame.add(panel);
 //        frame.setContentPane(panel);
 
         m = new Music();
         m.startMusic();
-      
+
         JButton startMusic = new JButton("Play Music");
         startMusic.addActionListener(e -> m.startMusic());
         startMusic.setPreferredSize(new Dimension(100,30));
