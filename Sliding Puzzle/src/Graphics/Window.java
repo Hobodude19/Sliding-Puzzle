a7949989f68530ee30043a889c521bd49d9a7d35
SSithub <57548627+SSithub@users.noreply.github.com>
2020-01-05 01:38:07 -0500

    Added an inner panel

--- Sliding Puzzle/src/Graphics/Window.java
+++ Sliding Puzzle/src/Graphics/Window.java
@@ -7,17 +7,27 @@ import java.awt.*;
 
 public class Window {
     JFrame frame;
+    JPanel inner;
     Music m;
 
     public Window() {
+        
+        inner = new JPanel();
+        inner.setPreferredSize(new Dimension(800,800));
+        inner.setBackground(new Color(56,52,55));
+        inner.setVisible(true);
+        
         frame = new JFrame("Sliding Puzzle");
 
-        frame.getContentPane().setBackground(new Color(56,52,55));
+        frame.getContentPane().setBackground(new Color(75,79,87));
         frame.setSize(1200,900);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-
+        frame.setLayout(new GridBagLayout());
+        frame.add(inner,new GridBagConstraints());
+        
+        
         m = new Music();
         m.startMusic();
 
